cdc68218e071ff5d456a8d9a2278f295f4a6529d
Mozibix <opuzmoses@gmail.com>
2023-04-22 00:05:41 +0100

    chores: updated left_hero_section

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,8 +4,27 @@ import hero from "/images/herobg.png";
 const Welcome = () => {
   return (
     <div className="welcome">
-      {/* <img src={hero} alt="" /> */}
-      <h2>Welcome</h2>
+      <div className="welcome_inner">
+        <div className="welcome_left">
+          <div className="welcome_left_top">
+            <span>WELCOME</span>
+          </div>
+          <div className="welcome_left_bottom">
+            <p> Wedding Planner</p>
+          </div>
+
+          <button className="btn_more">LEARN MORE</button>
+        </div>
+        <div className="welcome_right">
+          <div className="welcome_right_quote">
+            <p>
+              The #1 ranked. Helped over 50,000 couples around the country and
+              we look forward to supporting you along your wedding planning
+              journey.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
